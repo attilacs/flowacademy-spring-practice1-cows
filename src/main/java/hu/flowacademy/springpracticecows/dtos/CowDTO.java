881f99893037ffc887e66e4537dad1583c1b6fde
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-15 22:18:57 +0200

    Add CowDTO

---
+++ src/main/java/hu/flowacademy/springpracticecows/dtos/CowDTO.java
@@ -0,0 +1,18 @@
+package hu.flowacademy.springpracticecows.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Data
+public class CowDTO {
+    @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{3}$")
+    private String sid;
+
+    @NotNull
+    private Integer age;
+
+    @NotNull
+    private Integer stableId;
+}
