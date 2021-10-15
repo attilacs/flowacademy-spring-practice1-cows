5082009a7e86b6898149081268b564b2630c17b4
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-15 21:02:51 +0200

    Initial commit

---
+++ src/main/java/hu/flowacademy/springpracticecows/SpringPracticeCowsApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.springpracticecows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeCowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeCowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/flowacademy/springpracticecows/SpringPracticeCowsApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.springpracticecows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeCowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
