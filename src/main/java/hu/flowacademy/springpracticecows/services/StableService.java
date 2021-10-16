5be95bb07542739c5820d411146dbcba67ce20dc
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 10:27:16 +0200

    Add addStable methot to StableService

--- src/main/java/hu/flowacademy/springpracticecows/services/StableService.java
+++ src/main/java/hu/flowacademy/springpracticecows/services/StableService.java
@@ -1,11 +1,22 @@
 package hu.flowacademy.springpracticecows.services;
 
+import hu.flowacademy.springpracticecows.dtos.StableDTO;
+import hu.flowacademy.springpracticecows.entities.Stable;
 import hu.flowacademy.springpracticecows.repositories.StableRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @AllArgsConstructor
 public class StableService {
     private final StableRepository stableRepository;
+
+    public Stable addStable(StableDTO stableDTO) {
+        Stable stable = new Stable();
+        stable.setAddress(stableDTO.getAddress());
+        stable.setCows(new ArrayList<>());
+        return stableRepository.save(stable);
+    }
 }
