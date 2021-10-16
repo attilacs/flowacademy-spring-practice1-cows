25ee53e770cb9c9d88959d839dd910da1f99c4a9
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 12:27:58 +0200

    Add constructor annotations to StableDTO

--- src/main/java/hu/flowacademy/springpracticecows/dtos/StableDTO.java
+++ src/main/java/hu/flowacademy/springpracticecows/dtos/StableDTO.java
@@ -1,10 +1,14 @@
 package hu.flowacademy.springpracticecows.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StableDTO {
     @NotEmpty
     private String address;
