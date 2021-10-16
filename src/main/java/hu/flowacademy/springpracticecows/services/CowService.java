26ffeae87525aebe069566e1e30b4f9953407c87
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 13:35:36 +0200

    Add addCow method to CowService

--- src/main/java/hu/flowacademy/springpracticecows/services/CowService.java
+++ src/main/java/hu/flowacademy/springpracticecows/services/CowService.java
@@ -1,11 +1,31 @@
 package hu.flowacademy.springpracticecows.services;
 
+import hu.flowacademy.springpracticecows.dtos.CowDTO;
+import hu.flowacademy.springpracticecows.entities.Cow;
+import hu.flowacademy.springpracticecows.entities.Stable;
 import hu.flowacademy.springpracticecows.repositories.CowRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class CowService {
     private final CowRepository cowRepository;
+    private final StableService stableService;
+
+    public CowDTO addCow(CowDTO cowDTO) {
+        Cow cow = new Cow();
+        cow.setAge(cowDTO.getAge());
+        cow.setSid(cowDTO.getSid());
+        Optional<Stable> stable = stableService.findStableById(cowDTO.getStableId());
+        if (stable.isEmpty()) {
+            throw new NoSuchElementException("Nincs ilyen istálló");
+        }
+        cow.setStable(stable.get());
+        cowRepository.save(cow);
+        return cowDTO;
+    }
 }
