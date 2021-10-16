7743fd04584217f42226eec56af57255f013b877
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 15:32:13 +0200

    Add addCow method to CowController

--- src/main/java/hu/flowacademy/springpracticecows/controllers/CowController.java
+++ src/main/java/hu/flowacademy/springpracticecows/controllers/CowController.java
@@ -1,13 +1,22 @@
 package hu.flowacademy.springpracticecows.controllers;
 
+import hu.flowacademy.springpracticecows.dtos.CowDTO;
 import hu.flowacademy.springpracticecows.services.CowService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/cow")
 @AllArgsConstructor
 public class CowController {
     private final CowService cowService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CowDTO addCow(@RequestBody @Valid CowDTO cowDTO) {
+        return cowService.addCow(cowDTO);
+    }
 }
