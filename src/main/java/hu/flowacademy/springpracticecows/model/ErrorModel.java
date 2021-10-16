1a9042e334a9d249b508f9d9132b0ed98428dd94
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 11:26:14 +0200

    Add constructor annotations to ErrorModel

--- src/main/java/hu/flowacademy/springpracticecows/model/ErrorModel.java
+++ src/main/java/hu/flowacademy/springpracticecows/model/ErrorModel.java
@@ -1,8 +1,12 @@
 package hu.flowacademy.springpracticecows.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorModel {
     private String message;
     private Object data;
