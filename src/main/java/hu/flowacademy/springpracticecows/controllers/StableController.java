20c7da6cb66ea8f6f1d65a23ce9983332410961a
Attila Csanyi <csanyiattila23@gmail.com>
2021-10-16 10:36:44 +0200

    Add addStable method to StableController

--- src/main/java/hu/flowacademy/springpracticecows/controllers/StableController.java
+++ src/main/java/hu/flowacademy/springpracticecows/controllers/StableController.java
@@ -1,13 +1,23 @@
 package hu.flowacademy.springpracticecows.controllers;
 
+import hu.flowacademy.springpracticecows.dtos.StableDTO;
+import hu.flowacademy.springpracticecows.entities.Stable;
 import hu.flowacademy.springpracticecows.services.StableService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/stable")
 @AllArgsConstructor
 public class StableController {
     private final StableService stableService;
+
+    @PostMapping
+    public ResponseEntity<StableDTO> addStable(@RequestBody @Valid StableDTO stableDTO) {
+        stableService.addStable(stableDTO);
+        return ResponseEntity.ok(stableDTO);
+    }
 }
